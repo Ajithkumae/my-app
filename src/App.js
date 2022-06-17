089dcc5b0a4008e6dd034d91fbd3e92fdce52c8b
Ajith Kumar <1037795@INLT1723-MAC.local>
2022-06-18 01:28:47 +0530

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
-import React, { useState } from "react";
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
+import React from "react";
 
-const dumi_data = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About/About";
+import CheckOut from "./components/CheckOut/CheckOut";
 
 const App = () => {
-  const [expenses, setExpnses] = useState(dumi_data);
-  const addExpenseHandler = (expense) => {
-    setExpnses((previousExpenses) => {
-      return [expense, ...previousExpenses];
-    });
-  };
-
   return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
-    </div>
+    <Routes>
+      <Route path="/" element={<CheckOut />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 };
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
@@ -6,6 +7,7 @@ const ExpenseForm = (props) => {
   const [enterdAmount, settEnterdAmount] = useState("");
   const [enterdDate, setEnterdDate] = useState("");
 
+  let navigate = useNavigate();
   const titleChangedHandler = (event) => {
     setEnterdtitle(event.target.value);
   };
@@ -25,51 +27,58 @@ const ExpenseForm = (props) => {
       amount: enterdAmount,
       date: new Date(enterdDate),
     };
-    props.onSaveExpenseDate(expenseData);
-    setEnterdtitle("");
-    settEnterdAmount("");
-    setEnterdDate("");
+    if (enterdtitle && enterdDate && enterdAmount) {
+      props.onSaveExpenseDate(expenseData);
+      setEnterdtitle("");
+      settEnterdAmount("");
+      setEnterdDate("");
+    } else {
+      alert("Add Field");
+    }
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls ">
-        <div className="new-expense__control">
-          <label>Tilte</label>
-          <input
-            type="text"
-            value={enterdtitle}
-            onChange={titleChangedHandler}
-          />
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls ">
+          <div className="new-expense__control">
+            <label>Tilte</label>
+            <input
+              type="text"
+              value={enterdtitle}
+              onChange={titleChangedHandler}
+            />
+          </div>
+
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enterdAmount}
+              onChange={amountChangedHandler}
+            />
+          </div>
+
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              step="2022-12-31"
+              value={enterdDate}
+              onChange={dateChangedHandler}
+            />
+          </div>
         </div>
 
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enterdAmount}
-            onChange={amountChangedHandler}
-          />
-        </div>
-
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            step="2022-12-31"
-            value={enterdDate}
-            onChange={dateChangedHandler}
-          />
-        </div>
-      </div>
-
-      <button type="submit" className="new-expnese__actions">
-        Add Expense
-      </button>
-    </form>
+        <button type="submit" className="new-expnese__actions">
+          Add Expense
+        </button>
+      </form>
+      <button onClick={() => navigate("/about")}>go to about </button>
+    </div>
   );
 };
 
